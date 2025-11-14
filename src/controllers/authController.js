3804c6fc846a4a6d56e41d7e3d76c9bb21ce9f36
Olexander Motyl <JustSanya1@gmail.com>
2025-11-14 16:38:28 +0100

    create name on register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,7 @@ import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 
 export const registerUser = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password, name } = req.body;
 
   const existingUser = await User.findOne({ email });
   if (existingUser) {
@@ -15,6 +15,7 @@ export const registerUser = async (req, res, next) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await User.create({
+    name,
     email,
     password: hashedPassword,
     savedRecipes: [],
