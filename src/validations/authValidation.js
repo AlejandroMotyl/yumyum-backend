3781fda836a4c37b7ea1255d6381e35353e23bb4
Olexander Motyl <JustSanya1@gmail.com>
2025-11-16 22:04:56 +0100

    update auth schemas

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -2,15 +2,15 @@ import { Joi, Segments } from 'celebrate';
 
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
-    name: Joi.string().max(16).required(),
-    email: Joi.string().email().max(128).required(),
-    password: Joi.string().min(8).max(128).required(),
+    name: Joi.string().trim().max(16).required(),
+    email: Joi.string().trim().email().max(128).required(),
+    password: Joi.string().trim().pattern(/^\S+$/).min(8).max(128).required(),
   }),
 };
 
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
-    email: Joi.string().email().max(128).required(),
-    password: Joi.string().max(128).required(),
+    email: Joi.string().trim().email().max(128).required(),
+    password: Joi.string().trim().pattern(/^\S+$/).max(128).required(),
   }),
 };
