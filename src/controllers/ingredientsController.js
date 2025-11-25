acc8f07bd25c197c5aabde587eb36bac12afaf5e
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-11-25 12:25:14 +0200

    sort of ingredients & categories

--- src/controllers/ingredientsController.js
+++ src/controllers/ingredientsController.js
@@ -3,7 +3,7 @@ import { Ingredient } from '../models/ingredient.js';
 
 export const getIngredients = async (req, res, next) => {
   try {
-    const result = await Ingredient.find();
+    const result = await Ingredient.find().sort({ name: 1 });
 
     if (!result || result.length === 0) {
       throw createHttpError(404, 'No ingredients found');
